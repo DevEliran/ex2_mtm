c36a71ab632828f0436f70777486128060f5cf7d
Eliran Turgeman <50831652+DevEliran@users.noreply.github.com>
2019-12-15 14:01:21 +0200

    Add files via upload

--- Survey.i
+++ Survey.i
@@ -1,13 +1,33 @@
-#TODO
+%module Survey
 %{
- #TODO
+ #include "Survey.h"
+ #define SURVEY_VEGAN 0
+#define SURVEY_VEGATERIAN 1
+#define SURVEY_OMNIVORE 2
+Survey SurveyCreateSurvey();
+SurveyReturnValue SurveyAddPerson(Survey Survey, int Id, int Age, bool Gender, int EatingHabits , int* Scores);
+int* SurveyQuerySurvey(Survey survey, int ChocolateType, bool Gender, int AgeMin, int AgeMax, int EatingHabits);
+void SurveyQueryDestroy(int* histogram);
+void SurveyDestroySurvey(Survey survey);
+int* SurveyCreateIntAr(unsigned int size);
+void SurveyDestoryIntAr(int* ar);
+void SurveySetIntArIdxVal (int* ar, unsigned int idx, int val);
+int SurveyGetIntArIdxVal(int* ar, unsigned int idx);
 %}
 
 typedef enum { SURVEY_ALLOCATION_FAILED, SURVEY_SUCCESS} SurveyReturnValue;
 
 typedef struct survey_t* Survey;
-
+#define SURVEY_VEGAN 0
+#define SURVEY_VEGATERIAN 1
+#define SURVEY_OMNIVORE 2
 Survey SurveyCreateSurvey();
-
-#TODO
+SurveyReturnValue SurveyAddPerson(Survey Survey, int Id, int Age, bool Gender, int EatingHabits , int* Scores);
+int* SurveyQuerySurvey(Survey survey, int ChocolateType, bool Gender, int AgeMin, int AgeMax, int EatingHabits);
+void SurveyQueryDestroy(int* histogram);
+void SurveyDestroySurvey(Survey survey);
+int* SurveyCreateIntAr(unsigned int size);
+void SurveyDestoryIntAr(int* ar);
+void SurveySetIntArIdxVal (int* ar, unsigned int idx, int val);
+int SurveyGetIntArIdxVal(int* ar, unsigned int idx);
 
--- pythonFuncs.py
+++ pythonFuncs.py
@@ -1,4 +1,5 @@
 from itertools import chain
+import Survey
 #Filters a survey and prints to screen the corrected answers:
 #old_survey_path: The path to the unfiltered survey
 def correct_myfile(old_survey_path):
@@ -9,10 +10,13 @@ def correct_myfile(old_survey_path):
     for index, i in enumerate(lines_to_copy):
         if index % 2 == 0:
             index_of_lines_to_copy.append(i)
-    with open("correct_"+old_survey_path, "a+") as new_file, open(mid_fixed, "r") as old_file:
+    with open("corrected_"+old_survey_path, "a+") as new_file, open(mid_fixed, "r") as old_file:
         lines = old_file.readlines()
         for k in index_of_lines_to_copy:
             new_file.write(lines[k])
+    with open("corrected_"+old_survey_path, 'r') as final_file:
+        content = final_file.read()
+    print(content.rstrip('\n'))
     new_file.close()
 
 
@@ -51,10 +55,8 @@ def countIdShow(file_path):
                 idx = lines_to_copy.index(data[0])
                 lines_to_copy[idx-1] = index
 
-    print(lines_to_copy)
     file.close()
     final_list=[*chain(*sorted(zip(lines_to_copy[::2], lines_to_copy[1::2]), key=lambda k:k[1]))]
-    print(final_list)
     return final_list
 
 
@@ -70,18 +72,32 @@ def isValidData(id, age, grades):
        except ValueError:
             continue
    return 'GOODATA'
+# ~~~~~~~~~~~~~~~~~~ Part1 DONE ~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~ Part2 ~~~~~~~~~~~~~~~~~~~~~
 
 #Returns a new Survey item with the data of a new survey file:
 #survey_path: The path to the survey
 def scan_survey(survey_path):
-    with open(survey_path,'r') as file:
-        content = file.read()
-    with open(survey_path+"_copy",'w') as file_copy:
-        file_copy.write(content)
-
-    file.close()
-    file_copy.close()
-    return survey_path+"_copy"
+    with open(survey_path, 'r') as file:
+        new_survey = Survey.SurveyCreateSurvey()
+        for line in file:
+            data = line.split(' ')
+            grades_array = list(filter(lambda x: x != '', data[4:]))
+            grades_int = Survey.SurveyCreateIntAr(len(grades_array))
+            for idx, score in enumerate(grades_array):
+                Survey.SurveySetIntArIdxVal(grades_int, idx, int(score))
+            if data[3] == 'Man':
+                gender = True
+            else:
+                gender = False
+            if data[1] == 'Vegan':
+                habits = Survey.SURVEY_VEGAN
+            elif data[1] == 'Vegaterian':
+                habits = Survey.SURVEY_VEGATERIAN
+            else:
+                habits = Survey.SURVEY_OMNIVORE
+            Survey.SurveyAddPerson(new_survey, int(data[0]), int(data[2]), gender, habits, grades_int)
+    return new_survey
 
 #Prints a python list containing the number of votes for each rating of a group according to the arguments
 #s: the data of the Survey object
@@ -91,18 +107,23 @@ def scan_survey(survey_path):
 #max_age: the maximum age of the group (a number)
 #eating_habits: the eating habits of the group (string of "Omnivore", "Vegan" or "Vegetarian")
 def print_info(s, choc_type, gender, min_age, max_age, eating_habits):
-    rate_list = []
-    with open(s, 'r') as file:
-        for index,line in enumerate(file):
-            data= line.split(' ')
-            grades = data[4:]
-            [grades.remove(rate) for rate in grades if rate == '']
-            if data[3] == gender and int(min_age)<=int(data[2])<=int(max_age) and data[1] == eating_habits:
-                [rate_list.append(grade) for index, grade in enumerate(grades) if int(index) == int(choc_type)]
-    print(rate_list)
-    file.close()
+    if gender == 'Man':
+        gender_bool = True
+    else:
+        gender_bool = False
+    if eating_habits == 'Vegan':
+        habits = Survey.SURVEY_VEGAN
+    elif eating_habits == 'Vegeterian':
+        habits = Survey.SURVEY_VEGATERIAN
+    else:
+        habits = Survey.SURVEY_OMNIVORE
+    result = Survey.SurveyQuerySurvey(s, choc_type, gender_bool, min_age, max_age, habits)
+    result_array=[]
+    for idx,score in enumerate(result):
+        result_array.append(Survey.SurveyGetIntArIdxVal(result, idx))
+    print(result_array)
 
 #Clears a Survey object data
 #s: the data of the Survey object
 def clear_survey(s):
-    open(s, 'w').close()
+    Survey.SurveyDestroySurvey(s)
